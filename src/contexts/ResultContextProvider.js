88f345513cbe314ba006c421d536edf52bb8d237
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-10-27 14:09:07 +0545

    finishing up google search clone

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -17,7 +17,7 @@ export const ResultContextProvider = ({ children }) => {
       headers: {
         "x-user-agent": "desktop",
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": "a19d5d874dmsh45d4686739a1c5cp123464jsn0f9bc619a109",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     });
 
