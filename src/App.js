b4868a9f29ee10170960b4d4cfd49727005753a1
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-10-27 09:46:20 +0545

    added routes, navbar, footer components, implemented dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { Routes } from "./components/Routes";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div>
-      <Navbar />
-      <Routes />
-      <Footer />
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 };
